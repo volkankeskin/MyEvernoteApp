ce04dbbc3c11b115b08daaed09f368ec5a330530
VOLKAN KESKİN <37207190+volkankeskin@users.noreply.github.com>
2021-10-24 16:36:48 +0300

    Update Program.cs
    write note

--- ConsoleApp4/ConsoleApp4/Program.cs
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+            //not buraya yapılacak işlem var.
         }
     }
 }
